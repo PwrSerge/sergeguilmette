47fb43af1f51f64dd8fc2999e190add121f8ba48
Serge Guilmette <sergeguilmette@gmail.com>
2015-02-25 11:39:48 -0500

    test

--- gulpfile.js
+++ gulpfile.js
@@ -416,7 +416,7 @@ var options = {
     remoteUrl: "https://github.com/PwrSerge/serguilmette.github.io.git",
     branch: "gh-pages"};
 
-gulp.task('deploy', function (options) {
+gulp.task('deploy', function () {
     return gulp.src('./dist/**/*')
                 .pipe(deploy());
 });
